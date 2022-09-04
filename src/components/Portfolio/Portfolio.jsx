61e1db642191c0a433e3a390bf4a1dcf675ca700
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-09-04 00:11:45 -0500

    weather app added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,26 +1,12 @@
 import my_image from '../Portfolio_Images/my_image.jpg';
 import GitHub from '../Portfolio_Images/GitHub.png';
 import My_CV from '../Portfolio_Images/My_CV.pdf'
-import app_dogs_1 from '../App_dogs_images/app_dogs1.png';
-import app_dogs_2 from '../App_dogs_images/app_dogs2.png';
-import app_dogs_3 from '../App_dogs_images/app_dogs3.png';
-import app_dogs_4 from '../App_dogs_images/app_dogs4.png';
-import app_dogs_5 from '../App_dogs_images/app_dogs5.png';
-import synnet_image1 from '../Synnet_app_images/synnet_image1.png';
-import synnet_image2 from '../Synnet_app_images/synnet_image2.png';
-import synnet_image3 from '../Synnet_app_images/synnet_image3.png';
-import synnet_image4 from '../Synnet_app_images/synnet_image4.png';
-import synnet_image5 from '../Synnet_app_images/synnet_image5.png';
-import { MdArrowForwardIos } from 'react-icons/md';
-import { MdArrowBackIosNew } from 'react-icons/md';
 import { GoMarkGithub } from 'react-icons/go';
 import { BsLinkedin } from 'react-icons/bs';
 import { ImMail } from 'react-icons/im';
 import { SiJavascript } from 'react-icons/si';
 import { IoLogoNodejs } from 'react-icons/io';
 import { GrReactjs } from 'react-icons/gr';
-import './Portfolio.css'
-import { useState } from 'react';
 import { SiRedux } from 'react-icons/si';
 import { ImHtmlFive } from 'react-icons/im';
 import { IoLogoCss3 } from 'react-icons/io';
@@ -28,77 +14,24 @@ import { SiSequelize } from 'react-icons/si';
 import { SiExpress } from 'react-icons/si';
 import { SiPostgresql } from 'react-icons/si';
 import { DiScrum } from 'react-icons/di';
+import Module_Projects from '../Mudule_Projects/Module_Projects';
+import { 
+  Array_App_dogs, 
+  Array_App_Synnet, 
+  Array_Weather_App, 
+  Synnet_Descriptions, 
+  App_Dogs_Descriptions, 
+  Weather_App_Descriptions, 
+  About_Me, 
+} from '../Projects_Description/Projects_Description';
+
+
+import './Portfolio.css'
+import { useEffect } from 'react';
 
 const Portfolio = ({state, idiom}) => {
 
   
-  const App_dogs = [
-    app_dogs_1,
-    app_dogs_2,
-    app_dogs_3,
-    app_dogs_4,
-    app_dogs_5,
-  ]
-  const App_Synnet = [
-    synnet_image1,
-    synnet_image2,
-    synnet_image3,
-    synnet_image4,
-    synnet_image5,
-  ]
-  const [AppDogsImage, setAppDogsImage ] = useState(app_dogs_1);
-  const [AppSynnetImage, setAppSynnetImage ] = useState(synnet_image1);
-  const [OpenGitAndDeployDogs, setOpenGitAndDeployDogs ] = useState(false);
-  const [OpenGitAndDeploySynnet, setOpenGitAndDeploySynnet ] = useState(false);
-  const [HitHub, setHitHub ] = useState(null);
-  const [Deploy, setDeploy ] = useState(null);
-
-  const HandleClickAppDogs = (value) => {
-    if(value === 'ahead') {
-      if(AppDogsImage === App_dogs[0]) setAppDogsImage(App_dogs[1])
-      else if(AppDogsImage === App_dogs[1]) setAppDogsImage(App_dogs[2])
-      else if(AppDogsImage === App_dogs[2]) setAppDogsImage(App_dogs[3])
-      else if(AppDogsImage === App_dogs[3]) setAppDogsImage(App_dogs[4])
-      else if(AppDogsImage === App_dogs[4]) setAppDogsImage(App_dogs[0])
-    } else {
-      if(AppDogsImage === App_dogs[0]) setAppDogsImage(App_dogs[4])
-      else if(AppDogsImage === App_dogs[1]) setAppDogsImage(App_dogs[0])
-      else if(AppDogsImage === App_dogs[2]) setAppDogsImage(App_dogs[1])
-      else if(AppDogsImage === App_dogs[3]) setAppDogsImage(App_dogs[2])
-      else if(AppDogsImage === App_dogs[4]) setAppDogsImage(App_dogs[3])
-
-    }
-  }
-
-  const HandleClickAppSynnet = (value) => {
-    if(value === 'ahead') {
-      if(AppSynnetImage === App_Synnet[0]) setAppSynnetImage(App_Synnet[1])
-      else if(AppSynnetImage === App_Synnet[1]) setAppSynnetImage(App_Synnet[2])
-      else if(AppSynnetImage === App_Synnet[2]) setAppSynnetImage(App_Synnet[3])
-      else if(AppSynnetImage === App_Synnet[3]) setAppSynnetImage(App_Synnet[4])
-      else if(AppSynnetImage === App_Synnet[4]) setAppSynnetImage(App_Synnet[0])
-    } else {
-      if(AppSynnetImage === App_Synnet[0]) setAppSynnetImage(App_Synnet[4])
-      else if(AppSynnetImage === App_Synnet[1]) setAppSynnetImage(App_Synnet[0])
-      else if(AppSynnetImage === App_Synnet[2]) setAppSynnetImage(App_Synnet[1])
-      else if(AppSynnetImage === App_Synnet[3]) setAppSynnetImage(App_Synnet[2])
-      else if(AppSynnetImage === App_Synnet[4]) setAppSynnetImage(App_Synnet[3])
-
-    }
-  }
-
-  const HandleSetNextPage = (value) => {
-    if(value === 'AppDogs') {
-      setHitHub('https://github.com/DF27ARTS/Henry-PI-Dogs')
-      setDeploy('https://henry-pi-dogs-d26rn64ql-df27arts.vercel.app/')
-      setOpenGitAndDeployDogs(!OpenGitAndDeployDogs)
-    }
-    else if(value === 'SynnetApp') {
-      setHitHub('https://github.com/Nicrosi/Proyecto-Final')
-      setDeploy('https://proyecto-final-rho-three.vercel.app/')
-      setOpenGitAndDeploySynnet(!OpenGitAndDeploySynnet)
-    }
-  }
 
   return(
     <div 
@@ -124,41 +57,19 @@ const Portfolio = ({state, idiom}) => {
                     idiom ?
                     (
                       <>
-                        <p>Hola!</p>
-
-                        <p>Soy un desarrollador Full stack, cuando decidí hacer el bootcamp de henery 
-                        y termine la cursada me enamore de la programación. 
-                        Me gustan los retos y aprender cosas nuevas por eso aver cursar henry 
-                        fue una gran y maravillosa experiencia donde aprendí varias de las 
-                        tecnologías mas usadas en el mundo tech</p>
-  
-                        <p>Me gustaría tener la oportunidad de seguir aprendiendo nuevas tecnologías 
-                        y seguir mejorando los conocimientos aprendidos en el Bootcamp.</p>
-                        
-                        <p>Soy una persona curiosa con ganas de aprende y seguir mejorando,
-                        me gusta el trabajo en equipo y espero encontrar un lugar donde pueda seguir 
-                        desarrollando mis habilidades y donde pueda contribuir con mis conocimientos 
-                        además de seguir creciendo como programador y también como persona</p>
+                        <p>{About_Me.greeting_spanish}</p>
+                        <p>{About_Me.description1_spanish}</p>
+                        <p>{About_Me.description2_spanish}</p>
+                        <p>{About_Me.description3_spanish}</p>
                       </>
                     )
                     :
                     (
                       <>
-                        <p>Hi!</p>
-
-                        <p>I am a full stack developer, when I decided to do henery's bootcamp 
-                          and I finished the course I fell in love with programming. 
-                          I like challenges and learn new things, that's why studying in Henry 
-                          was a great and wonderful experience where I learned several of the most 
-                          used technologies in the tech world.</p>
-
-                        <p>I would like to have the opportunity to continue learning new technologies 
-                          and continue improving the knowledge learned in the Bootcamp.</p>
-
-                        <p>I am a curious person who wants to learn and keep improving.
-                          I like teamwork and I hope to find a place where I can continue 
-                          developing my skills and where I can contribute with my knowledge 
-                          as well as continue to grow as a programmer and also as a person.</p>
+                        <p>{About_Me.greeting_english}</p>
+                        <p>{About_Me.description1_english}</p>
+                        <p>{About_Me.description2_english}</p>
+                        <p>{About_Me.description3_english}</p>
                       </>
                     )
                   }
@@ -169,143 +80,40 @@ const Portfolio = ({state, idiom}) => {
             <div id='projects' className='Projects_title' >{ idiom ? 'Proyectos' : 'Projects'}</div>
             <div className="Projects" >
               <div className="single_project">
-              <h2 className='title' >App Dogs</h2>
-              <h3>
-                <ul>
-                  <div className='apps_infomation' >
-                    {
-                      idiom ?
-                      'Aplicación donde podras ver diferentes tipos de perros.' :
-                      'Application where you can see different types of dogs.'
-                    }
-                  </div>
-                  <li className='apps_infomation' >
-                    { 
-                      idiom ? 'Cuenta con filtros para filtrar por rázas y por temperamentos,' :
-                      'It has filters to filter by race and temperament,'
-                    }
-                  </li>
-                  <li className='apps_infomation' >
-                    {
-                      idiom ?
-                      'Posibilidad de ordenar por nombre de forma ascendente y descendente y tambíen ordenar por peso,' :
-                      'Ability to sort by name ascending and descending and also order by weight,'
-                    }
-                  </li>
-                  <li className='apps_infomation' >
-                    {
-                      idiom ?
-                      'Buscador para encontrar los perros por nombre,' :
-                      'Search engine to find dogs by name,'
-                    }
-                  </li>
-                  <li className='apps_infomation' >
-                    {
-                      idiom ?
-                      'sección de favoritos y un formulario para que puedas crear tu propio perrito' :
-                      'favorites section and a form so you can create your own dog'
-                    }
-                  </li>
-                </ul>
-              </h3>
-              <div className={ state ? "cont_Apps" : "cont_Apps cont_Apps_night"}>
-                <img onClick={()=>HandleSetNextPage('AppDogs')} className="apps_image" src={AppDogsImage} alt='app-images' />
-                <MdArrowForwardIos
-                  onClick={()=>HandleClickAppDogs('ahead')}
-                  className='Apps_images_buttons right '
+                <Module_Projects
+                  state={state}
+                  Array={Array_App_dogs}
+                  GitHub={"https://github.com/DF27ARTS/Henry-PI-Dogs"}
+                  Deploy={"https://henry-pi-dogs-d26rn64ql-df27arts.vercel.app/"}
+                  GitHubIcon={GitHub}
+                  idiom={idiom}
+                  Descriptions={App_Dogs_Descriptions}
                 />
-                <MdArrowBackIosNew
-                  onClick={()=>HandleClickAppDogs()}
-                  className='Apps_images_buttons left '
-                />
-                <div className={ OpenGitAndDeployDogs ? "next_page" : "next_page_block"}>
-                    <div className="page">
-                      <button className='git_and_deploy_link' >GitHub</button>
-                      <a href={HitHub} >
-                        <img className='next_page_img' src={GitHub} alt='GitHub' />
-                      </a>
-                    </div>
-                    <div className="page">
-                      <button className='git_and_deploy_link' >Deploy</button>
-                      <a href={Deploy} >
-                        <img className='next_page_img' src={app_dogs_1} alt='GitHub' />
-                      </a>
-                    </div>
-                </div>
               </div>
-              </div>
-
-
-
 
               <div className="single_project">
-              <h2 className='title' >Synnet</h2>
-              <h3>
-                <ul>
-                  <div className='apps_infomation' >
-                    {
-                      idiom ?
-                      'Aplicación que te permite crear y participar en torneos de tenis .' :
-                      'Application that allows you to create and participate in tennis tournaments.'
-                    }
-                  </div>
-                  <li className='apps_infomation' >
-                    { 
-                      idiom ? 'Contiene filtros que te permiten filtrar usuarios por nombre, genero y categoria,' :
-                      'Contains filters that allow you to filter users by name, gender and category,'
-                    }
-                  </li>
-                  <li className='apps_infomation' >
-                    {
-                      idiom ?
-                      'Tambien contiene galeria donde podras ver las imagenes mas destacadas de los diferentes torneos' :
-                      'It also contains a gallery where you can see the most outstanding images of the different tournaments'
-                    }
-                  </li>
-                  <li className='apps_infomation' >
-                    {
-                      idiom ?
-                      'Panel de administración para editar los usuarios, torneos entre otros, Dashboard para visualisar el estado de el torneo' :
-                      'Administration panel to edit users, tournaments among others, Dashboard to view the status of the tournament'
-                    }
-                  </li>
-                  <li className='apps_infomation' >
-                    {
-                      idiom ?
-                      'Metodo de pago, login  y registro, sección de patrocinadores, ChatBot para los usuarios que los guiara en el proceso de resgitro y pago de subtorneos' :
-                      'Payment method, login and registration, sponsors section, ChatBot for users that will guide them in the process of registration and payment of subtournaments'
-                    }
-                  </li>
-                </ul>
-              </h3>
-              <div className={ state ? "cont_Apps" : "cont_Apps cont_Apps_night"}>
-                <img onClick={()=>HandleSetNextPage('SynnetApp')} className="apps_image" src={AppSynnetImage} alt='app-images' />
-                <MdArrowForwardIos
-                  onClick={()=>HandleClickAppSynnet('ahead')}
-                  className='Apps_images_buttons right '
+                <Module_Projects
+                  state={state}
+                  Array={Array_App_Synnet}
+                  GitHub={"https://github.com/Nicrosi/Proyecto-Final"}
+                  Deploy={"https://proyecto-final-rho-three.vercel.app/"}
+                  GitHubIcon={GitHub}
+                  idiom={idiom}
+                  Descriptions={Synnet_Descriptions}
                 />
-                <MdArrowBackIosNew
-                  onClick={()=>HandleClickAppSynnet()}
-                  className='Apps_images_buttons left '
-                />
-                <div className={ OpenGitAndDeploySynnet ? "next_page" : "next_page_block"}>
-                    <div className="page">
-                      <button className='git_and_deploy_link' >GitHub</button>
-                    <a href={HitHub} >
-                      <img className='next_page_img' src={GitHub} alt='GitHub' />
-                    </a>
-                    </div>
-                    <div className="page">
-                      <button className='git_and_deploy_link' >Deploy</button>
-                      <a href={Deploy} >
-                        <img className='next_page_img' src={synnet_image1} alt='GitHub' />
-                      </a>
-                    </div>
-                </div>
-              </div>
               </div>
 
-
+              <div className="single_project">
+                <Module_Projects
+                  state={state}
+                  Array={Array_Weather_App}
+                  GitHub={"https://github.com/DF27ARTS/Weather_App"}
+                  Deploy={"https://weather-dg4hdgpg6-df27arts.vercel.app/"}
+                  GitHubIcon={GitHub}
+                  idiom={idiom}
+                  Descriptions={Weather_App_Descriptions}
+                />
+              </div>
 
             </div>
               <div id='tecnologies' className="Tecnologies">
---
+++ src/components/Projects_Description/Projects_Description.js
@@ -0,0 +1,115 @@
+import app_dogs_1 from '../App_dogs_images/app_dogs1.png';
+import app_dogs_2 from '../App_dogs_images/app_dogs2.png';
+import app_dogs_3 from '../App_dogs_images/app_dogs3.png';
+import app_dogs_4 from '../App_dogs_images/app_dogs4.png';
+import app_dogs_5 from '../App_dogs_images/app_dogs5.png';
+
+import synnet_image1 from '../Synnet_app_images/synnet_image1.png';
+import synnet_image2 from '../Synnet_app_images/synnet_image2.png';
+import synnet_image3 from '../Synnet_app_images/synnet_image3.png';
+import synnet_image4 from '../Synnet_app_images/synnet_image4.png';
+import synnet_image5 from '../Synnet_app_images/synnet_image5.png';
+
+import weather_app_img1 from '../Weather_app_images/weather_app_img1.jpg';
+import weather_app_img2 from '../Weather_app_images/weather_app_img2.jpg';
+import weather_app_img3 from '../Weather_app_images/weather_app_img3.jpg';
+import weather_app_img4 from '../Weather_app_images/weather_app_img4.jpg';
+import weather_app_img5 from '../Weather_app_images/weather_app_img5.jpg';
+import weather_app_img6 from '../Weather_app_images/weather_app_img6.jpg';
+
+
+export const Array_App_dogs = [
+  app_dogs_1,
+  app_dogs_2,
+  app_dogs_3,
+  app_dogs_4,
+  app_dogs_5,
+]
+
+export const Array_App_Synnet = [
+  synnet_image1,
+  synnet_image2,
+  synnet_image3,
+  synnet_image4,
+  synnet_image5,
+]
+
+export const Array_Weather_App = [
+  weather_app_img1,
+  weather_app_img2,
+  weather_app_img3,
+  weather_app_img4,
+  weather_app_img5,
+  weather_app_img6,
+]
+
+export const Synnet_Descriptions = {
+  title: "Synnet",
+  description1_english: "Application that allows you to create and participate in tennis tournaments.",
+  description1_spanish: "Aplicación que te permite crear y participar en torneos de tenis .",
+
+  description2_english: "Contains filters that allow you to filter users by name, gender and category,",
+  description2_spanish: "Contiene filtros que te permiten filtrar usuarios por nombre, genero y categoria,",
+
+  description3_english: "It also contains a gallery where you can see the most outstanding images of the different tournaments",
+  description3_spanish: "Tambien contiene galeria donde podras ver las imagenes mas destacadas de los diferentes torneos",
+
+  description4_english: "Administration panel to edit users, tournaments among others, Dashboard to view the status of the tournament",
+  description4_spanish: "Panel de administración para editar los usuarios, torneos entre otros, Dashboard para visualisar el estado de el torneo",
+
+  description5_english: "Payment method, login and registration, sponsors section, ChatBot for users that will guide them in the process of registration and payment of subtournaments",
+  description5_spanish: "Metodo de pago, login  y registro, sección de patrocinadores, ChatBot para los usuarios que los guiara en el proceso de resgitro y pago de subtorneos",
+}
+
+export const App_Dogs_Descriptions = {
+  title: "App Dogs",
+  description1_english: "Application where you can see different types of dogs.",
+  description1_spanish: "Aplicación donde podras ver diferentes tipos de perros.",
+
+  description2_english: "It has filters to filter by race and temperament,",
+  description2_spanish: "Cuenta con filtros para filtrar por rázas y por temperamentos,",
+
+  description3_english: "Ability to sort by name ascending and descending and also order by weight,",
+  description3_spanish: "Posibilidad de ordenar por nombre de forma ascendente y descendente y tambíen ordenar por peso,",
+
+  description4_english: "Search engine to find dogs by name,",
+  description4_spanish: "Buscador para encontrar los perros por nombre,",
+
+  description5_english: "favorites section and a form so you can create your own dog",
+  description5_spanish: "sección de favoritos y un formulario para que puedas crear tu propio perrito",
+}
+
+export const Weather_App_Descriptions = {
+  title: "Weather App",
+  description1_english: "Application to see the weather of different cities",
+  description1_spanish: "Aplicación para ver el clima de diferentes ciudades",
+
+  description2_spanish: "Cuenta con sección de Favoritos y detalle",
+  description1_english: "It has a Favorites section and detail",
+
+  description3_spanish: "Creada con el proposito de mejorar mis habilidades con React y TypeScript",
+  description1_english: "Created with the purpose of improving my skills with React and TypeScript"
+}
+
+export const About_Me = {
+  greeting_spanish: "Hola!",
+  greeting_english: "Hi!",
+
+  description1_spanish: "Soy un desarrollador Full stack, cuando decidí hacer el bootcamp de henery y termine la cursada me enamore de la programación. Me gustan los retos y aprender cosas nuevas por eso aver cursar henry fue una gran y maravillosa experiencia donde aprendí varias de las tecnologías mas usadas en el mundo tech",
+  description1_english: "I am a full stack developer, when I decided to do henery's bootcamp and I finished the course I fell in love with programming. I like challenges and learn new things, that's why studying in Henry was a great and wonderful experience where I learned several of the most used technologies in the tech world.",
+
+  description2_spanish: "Me gustaría tener la oportunidad de seguir aprendiendo nuevas tecnologías y seguir mejorando los conocimientos aprendidos en el Bootcamp.",
+  description2_english: "I would like to have the opportunity to continue learning new technologies and continue improving the knowledge learned in the Bootcamp.",
+
+  description3_spanish: "Soy una persona curiosa con ganas de aprende y seguir mejorando,me gusta el trabajo en equipo y espero encontrar un lugar donde pueda seguir desarrollando mis habilidades y donde pueda contribuir con mis conocimientos además de seguir creciendo como programador y también como persona",
+  description3_english: "I am a curious person who wants to learn and keep improving.I like teamwork and I hope to find a place where I can continue developing my skills and where I can contribute with my knowledge as well as continue to grow as a programmer and also as a person."
+}
+
+export const Descriptions = {
+  Array_App_dogs,
+  Array_App_Synnet,
+  Synnet_Descriptions,
+  App_Dogs_Descriptions,
+  About_Me,
+}
+
